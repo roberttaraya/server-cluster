54611d37c4aeb7b905184766cb23945cf8dbec52
rtaraya <rtaraya@us.ibm.com>
2017-03-07 13:10:02 -0600

    add a 'health-check' endpoint

--- service1.js
+++ service1.js
@@ -1,8 +1,10 @@
 module.exports = function() {
+  const healthCheck = require('./config/service1/health-check')
   const people = require('./config/service1/people')
   const places = require('./config/service1/places')
 
   return {
+    "health-check": healthCheck,
     people: people.data,
     places: places.data
   }
